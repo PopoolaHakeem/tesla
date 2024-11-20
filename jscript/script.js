02b9b7250ce169686cd8d387606581ec875bf098
PopoolaHakeem <hakeempopoola04@gmail.com>
2024-11-20 02:52:26 +0100

    new update

--- jscript/script.js
+++ jscript/script.js
@@ -2,14 +2,21 @@
 var menuicon = document.querySelector('#onclick')
 var mobilenav = document.querySelector('.mobile-nav')
 var cancelicon = document.querySelector('.cancel-icon')
+var cancel = document.querySelector('.cancel')
 
 menuicon.onclick = function() {
     menuicon.style.display = 'none'
     mobilenav.style.display = 'block'
     cancelicon.style.display = 'block'
 }
-// cancelicon.onclick = function () {
-//     mobilenav.style.display = 'none'
-//     cancelicon.style.display = 'none'
-//     menuicon.style.display = 'block'
-// } 
+cancelicon.onclick = function () {
+    mobilenav.style.display = 'none'
+    cancelicon.style.display = 'none'
+    menuicon.style.display = 'block'
+} 
+
+cancel.onclick = function() {
+    mobilenav.style.display = 'none'
+    cancel.style.display = 'cancel'
+    menuicon.style.display = 'block'
+}
